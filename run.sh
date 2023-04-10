1b5be4e7b5888150e6ec0e2d7c3327b04da68b37
Youtan YIN <54923393+cuteyyt@users.noreply.github.com>
2023-04-11 02:13:23 +0800

    Initial commit

---
+++ run.sh
@@ -0,0 +1,28 @@
+# SAM predict and lama dataset preparation
+cd prior/segment-angthing || exit
+python preprocess.py
+
+# Lama inpainting
+cd prior/lama || exit
+TORCH_HOME=$(pwd)
+PYTHONPATH=$(pwd)
+export TORCH_HOME && export PYTHONPATH
+
+python3 bin/predict.py \
+  model.path="$(pwd)"/../../ckpts/lama/big-lama \
+  indir="$(pwd)"/../../data/statue-sam/lama \
+  outdir="$(pwd)"/../../data/statue-sam/images \
+  refine=True
+
+# without refine
+python3 bin/predict.py \
+  model.path="$(pwd)"/../../ckpts/lama/big-lama \
+  indir="$(pwd)"/../../data/statue-sam/lama \
+  outdir="$(pwd)"/../../data/statue-sam/images
+
+# Train images before delete using nerf
+CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/statue.txt
+
+# Train images after delete using nerf
+# TODO: code for copy cam params
+CUDA_VISIBLE_DEVICES=1 python run_nerf.py --config configs/statue_delete.txt
