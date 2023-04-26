077b0682604fab315734dc8bc788df80e4e0a254
Youtan YIN <54923393+cuteyyt@users.noreply.github.com>
2023-04-26 21:09:24 +0800

    Update spinnerf_dataset/2

--- datasets/pre_sam.py
+++ datasets/pre_sam.py
@@ -27,6 +27,7 @@ filename_convert_dict = {
     },
     'spinnerf_dataset': {
         '2': None,
+        '3': None
     }
 }
 
@@ -91,7 +92,7 @@ def handle_imgs(in_dir, out_dir, **kwargs):
     in_paths = sorted(in_paths)
 
     if 'num_imgs' in kwargs:
-        in_paths = in_paths[:kwargs['num_imgs']]
+        in_paths = in_paths[kwargs['num_imgs']:]
 
     os.makedirs(out_dir, exist_ok=True)
     for in_path in tqdm(in_paths):
--- scripts/pipeline/run.sh
+++ scripts/pipeline/run.sh
@@ -1,10 +1,12 @@
 # nerf_llff_data
 sh scripts/pipeline/gen_lama_prior.sh nerf_llff_data room data
-sh scripts/pipeline/gen_lama_prior.sh nerf_llff_data horns data # not satisfactory
+sh scripts/pipeline/gen_lama_prior.sh nerf_llff_data horns data
 sh scripts/pipeline/gen_lama_prior.sh nerf_llff_data fortress data
 
 # llff_real_iconic
 sh scripts/pipeline/gen_lama_prior.sh llff_real_iconic data5_piano data
 
 # spinnerf_dataset
+# TODO: cam params have redundancy
 sh scripts/pipeline/gen_lama_prior.sh spinnerf_dataset 2 data
+sh scripts/pipeline/gen_lama_prior.sh spinnerf_dataset 3 data
