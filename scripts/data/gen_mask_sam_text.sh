fd279e160fa3885e950c6a354922b2602104159f
krisj <ZHOUJIE001@e.ntu.edu.sg>
2023-05-07 16:36:13 +0800

    refine text prompt

---
+++ scripts/data/gen_mask_sam_text.sh
@@ -0,0 +1,19 @@
+# Config params
+DATASET=$1
+SCENE=$2
+DATADIR=$3
+
+set -e
+
+# SAM predict with text prompt
+# --hybrid for text-points pipeline
+export CUDA_VISIBLE_DEVICES=1
+python datasets/pre_text_prompt.py \
+  --config prior/Grounded-Segment-Anything/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
+  --grounded_checkpoint ckpts/grounded_sam/groundingdino_swint_ogc.pth \
+  --sam_checkpoint ckpts/sam/sam_vit_h_4b8939.pth \
+  --dataset "${DATASET}" \
+  --scene "${SCENE}" \
+  --text_prompt_json configs/prepare_data/sam_text.json \
+  --device "cuda" \
+  #--hybrid
